c22e8df15fe06a446b702a01e7c25a10a9d50867
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-11-17 11:53:03 +0100

    Sort yarn

--- js/yarn-page.js
+++ js/yarn-page.js
@@ -2,30 +2,30 @@ const yarnImages = document.querySelectorAll('.yarn-img-container');
 const hoverDisplays = document.querySelectorAll('.hover-display');
 
 const toggleHover = (index, show) => {
-  const display = hoverDisplays[index];
-  if (show) {
-    display.classList.remove('display-none');
-  } else {
-    display.classList.add('display-none');
-  }
+    const display = hoverDisplays[index];
+    if (show) {
+        display.classList.remove('display-none');
+    } else {
+        display.classList.add('display-none');
+    }
 };
 
 const hoverImg = () => {
-  yarnImages.forEach((img, index) => {
-    img.addEventListener('mouseover', () => {
-      toggleHover(index, true);
-    });
+    yarnImages.forEach((img, index) => {
+        img.addEventListener('mouseover', () => {
+        toggleHover(index, true);
+        });
 
-    img.addEventListener('mouseleave', () => {
-      toggleHover(index, false);
+        img.addEventListener('mouseleave', () => {
+        toggleHover(index, false);
+        });
     });
-  });
 };
 
 hoverImg();
 
 
-//filter
+//filter and sort
 
 const filterButtons = document.querySelectorAll('.filter-button');
 const listItems = document.querySelectorAll('.list-item'); 
@@ -46,13 +46,13 @@ const filterList = (event) => {
 		}
 	});
 
-  filterButtons.forEach(button => {
-    button.style.backgroundColor = '';
-  });
+    filterButtons.forEach(button => {
+        button.style.backgroundColor = '';
+    });
 
-  currentButton.style.backgroundColor = '#E25E3E';
+    currentButton.style.backgroundColor = '#E25E3E';
 
-	listItemsContainer.innerHTML = '';
+	listItemsContainer.textContent = '';
 	
 	filteredItems.forEach(item => {
 		listItemsContainer.appendChild(item);
@@ -61,4 +61,53 @@ const filterList = (event) => {
 
 filterButtons.forEach(filterButton => {
 	filterButton.addEventListener('click', filterList);
+});
+
+const sortButtons = document.querySelectorAll('.sort-button')
+
+let direction = -1;
+
+const sortList = (event) => {
+    const currentButton = event.currentTarget;
+
+    if (direction === 1) {
+        direction = -1;
+    } else {
+        direction = 1;
+    }
+
+    if (currentButton.dataset.sortBy === 'name'){
+    const sortedItems = [...listItems].sort((a, b) => {
+  
+        const order = (a.dataset.name > b.dataset.name) ? 1 : -1;
+  
+        return order * direction;
+    });
+    
+    listItemsContainer.textContent = '';
+ 
+    sortedItems.forEach(item => {
+        listItemsContainer.appendChild(item);
+    });
+
+    } else if (currentButton.dataset.sortBy === 'price'){
+        const sortedItems = [...listItems].sort((a, b) => {
+            const aAsNumber = parseInt(a.dataset.price);
+            const bAsNumber = parseInt(b.dataset.price);
+
+            const order = (aAsNumber > bAsNumber) ? 1 : -1;
+
+            return order * direction; 
+        });
+
+        listItemsContainer.textContent = '';
+    
+        sortedItems.forEach(item => {
+            listItemsContainer.appendChild(item);
+        });
+    }
+}
+
+sortButtons.forEach(sortButton => {
+  sortButton.addEventListener('click', sortList);
 });
